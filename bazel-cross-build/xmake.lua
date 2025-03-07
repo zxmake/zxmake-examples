6567ce4ae41aeae03ea9dac63bc0ba5005e7a620
TOMOCAT <TOMO-CAT@github.com>
2025-03-07 16:14:19 +0800

    [Feature]: bazel examples

--- bazel-cross-build/xmake.lua
+++ bazel-cross-build/xmake.lua
@@ -1,4 +1,17 @@
+add_includedirs(".")
+
+target("bazel-cross-build.hello-time", function()
+    set_kind("object")
+    add_files("lib/hello-time.cc")
+end)
+
+target("bazel-cross-build.hello-greet", function()
+    set_kind("object")
+    add_files("main/hello-greet.cc")
+end)
+
 target("bazel-cross-build.hello-world", function()
     set_kind("binary")
-    add_files("src/hello-world.cc")
+    add_files("main/hello-world.cc")
+    add_deps("bazel-cross-build.hello-time", "bazel-cross-build.hello-greet")
 end)
