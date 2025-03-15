9285f7e3c915636ca12c1bd8ef462da13db5fe32
TOMOCAT <TOMO-CAT@github.com>
2025-03-15 11:52:16 +0800

    [Feature]: github actions

--- xmake/02-cross-build/muslcc/xmake.lua
+++ xmake/02-cross-build/muslcc/xmake.lua
@@ -12,10 +12,6 @@ set_toolchains("@muslcc")
 
 add_requires("zlib", "libogg", {system = false})
 
--- 为了生成 compile_commands.json
-add_sysincludedirs(
-    "/root/.xmake/packages/m/muslcc/20210202/f54cc74e8e224857a5b4d85f38f35406/arm-linux-musleabi/include")
-
 target("test", function()
     set_kind("binary")
     add_files("src/*.c")
