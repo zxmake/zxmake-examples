bbd009635c9f30035579ac78883ee326cc5c66dc
TOMOCAT <TOMO-CAT@github.com>
2025-03-19 23:50:38 +0800

    [Feature]: hermetic_cc_toolchain

---
+++ bazel/01-cross-build/hermetic_cc_toolchain/build.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+set -e
+
+function info() {
+  (>&2 printf "[\e[34m\e[1mINFO\e[0m] $*\n")
+}
+
+function error() {
+  (>&2 printf "[\033[0;31mERROR\e[0m] $*\n")
+}
+
+function warning() {
+  (>&2 printf "[\033[0;33mWARNING\e[0m] $*\n")
+}
+
+function ok() {
+  (>&2 printf "[\e[32m\e[1m OK \e[0m] $*\n")
+}
+
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+cd "${SCRIPT_DIR}"
+
+bazel build \
+    --platforms @zig_sdk//platform:linux_arm64 \
+    --extra_toolchains @zig_sdk//toolchain:linux_arm64_musl \
+    //main:hello-world
+
+# 生成 compile_commands.json
+# 需要非 root 权限, 没法在 github action 里跑
+if [ -z "${GITHUB_ACTIONS+x}" ]; then
+    info "not running in github actions"
+    bazel run @hedron_compile_commands//:refresh_all
+else
+    info "running in github actions, skip generating compile_commands.json"
+fi
+
+cd -
