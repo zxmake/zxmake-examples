dafc14d033ff686f9e7d97d0455b49e85054801d
TOMOCAT <TOMO-CAT@github.com>
2025-03-29 12:24:59 +0800

    [Examples]: cmake cuda target

---
+++ cmake/01-basic/L-cuda-target/build.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -e
+
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+BUILD_DIR="${SCRIPT_DIR}/build"
+
+rm "${BUILD_DIR}" -rf
+mkdir "${BUILD_DIR}"
+cd "${BUILD_DIR}"
+cmake ..
+make VERBOSE=1
+cd -
